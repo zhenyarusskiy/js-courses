683eb2d6ca668654e59d65757d8f0044e26108ce
Zhenya <russkiy.zhenya@gmail.com>
2019-07-09 22:03:00 +0300

    Real JS

--- packages/lecture-1/src/math.js
+++ packages/lecture-1/src/math.js
@@ -1,7 +1,11 @@
 export function sum(a, b) {
   // TODO: write function to pass tests
+  return (a + b);
 }
 
 export function powerWithClosure(exponent) {
   // TODO: write function to pass tests
+  return function pow2(value) {
+    return Math.pow(value, exponent);
+  }
 }
--- packages/lecture-1/src/transform.js
+++ packages/lecture-1/src/transform.js
@@ -1,3 +1,18 @@
 export function transformData(arr) {
   // TODO: write some code to make test work
+  if (!Array.isArray(arr)) { //Массив ли это?
+    throw new Error('is not Array');
+  }
+
+  if (!arr.length) { //Пустой ли??
+    throw new Error('Array is empty');
+  }
+
+  return arr.reduce((prev, current) => {
+    if (!current.key.includes('garbage')) {
+      prev[current.key] = current.value;
+    }
+
+    return prev;
+  }, {});
 }
